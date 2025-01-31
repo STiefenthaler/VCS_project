f1290123d3df5572b3b5a8e95cd6e6e1a5d78a27
Gaia Nardella <105795404+gaianardella@users.noreply.github.com>
2025-01-31 11:03:51 +0100

    Compute metrics on aggregated count and not for each class

--- evaluation.py
+++ evaluation.py
@@ -3,155 +3,104 @@ import argparse
 import os
 
 def process_files(video_name):
-
     video_name = str(video_name).split(".")[0]
-
-    # Define the video folder path
     current_path = os.getcwd()
     folder_path = f"{current_path}/{video_name}/"
 
-
-    # Read ground truth file
     with open(f"{current_path}/ground_truth/{video_name}_ground_truth.txt", 'r') as file:
-        truth = ast.literal_eval(file.read()) #transform file content into dictionary
+        truth = ast.literal_eval(file.read())
 
-    # Read all files in video folder
-    # Combine folder path and pattern to get matching files
     all_files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
 
-
-    # Iterate through the files and read their content
     for file_path in all_files:
-        content = ""
         with open(folder_path + file_path, "r") as file:
             print(f"Reading {file_path}...")
-            content = file.read()
-            content = content.strip(",") #remove last comma
+            content = file.read().strip(",")
             content = "{" + content + "}"
+            detected = ast.literal_eval(content)
 
-            detected = ast.literal_eval(content) #transform file content into dictionary
-        
-        #Find how many elements are detected at each time stamp
         detected_differences = {}
         truth_differences = {}
 
         previous_data = None
         for timestamp, current_data in detected.items():
-            if previous_data is None:
-                diff = {
-                    'incoming': {key: current_data['incoming'].get(key, 0) for key in current_data['incoming']},
-                    'outgoing': {key: current_data['outgoing'].get(key, 0) for key in current_data['outgoing']}
-                }
-
-            else:
-                diff = {
-                    'incoming': {key: current_data['incoming'].get(key, 0) - previous_data['incoming'].get(key, 0)
-                                for key in set(current_data['incoming']) | set(previous_data['incoming'])},
-                    'outgoing': {key: current_data['outgoing'].get(key, 0) - previous_data['outgoing'].get(key, 0)
-                                for key in set(current_data['outgoing']) | set(previous_data['outgoing'])}
-                }
+            diff = {key: {subkey: current_data[key].get(subkey, 0) - (previous_data[key].get(subkey, 0) if previous_data else 0)
+                          for subkey in set(current_data[key]) | (set(previous_data[key]) if previous_data else set())}
+                    for key in ['incoming', 'outgoing']}
             detected_differences[timestamp] = diff
             previous_data = current_data
 
         previous_data = None
         for timestamp, current_data in truth.items():
-            if previous_data is None:
-                diff = {
-                    'incoming': {key: current_data['incoming'].get(key, 0) for key in current_data['incoming']},
-                    'outgoing': {key: current_data['outgoing'].get(key, 0) for key in current_data['outgoing']}
-                }
-            else:
-                diff = {
-                    'incoming': {key: current_data['incoming'].get(key, 0) - previous_data['incoming'].get(key, 0)
-                                for key in set(current_data['incoming']) | set(previous_data['incoming'])},
-                    'outgoing': {key: current_data['outgoing'].get(key, 0) - previous_data['outgoing'].get(key, 0)
-                                for key in set(current_data['outgoing']) | set(previous_data['outgoing'])}
-                }
+            diff = {key: {subkey: current_data[key].get(subkey, 0) - (previous_data[key].get(subkey, 0) if previous_data else 0)
+                          for subkey in set(current_data[key]) | (set(previous_data[key]) if previous_data else set())}
+                    for key in ['incoming', 'outgoing']}
             truth_differences[timestamp] = diff
             previous_data = current_data
 
-
-        # Find total number of cars found by model and in ground truth to check if True Positive, False Positive and False negative are correct.
-        total_truth = truth[list(truth.keys())[-1]]
-        for key in total_truth.keys():
-            print(key)
-            for subkey in total_truth[key]:
-                print(subkey)
-
-                # Compute TP, FP, FN
-                TP, FP, FN = 0, 0, 0
-        
-                for timestamp in sorted(set(detected_differences) | set(truth_differences)):
-                    detected_diff = detected_differences.get(timestamp, {key:{subkey:0}})
-                    truth_diff = truth_differences.get(timestamp, {key:{subkey:0}})
-
-                    if subkey in detected_diff[key] or subkey in truth_diff[key]:
-                        detected_count = detected_diff[key].get(subkey, 0)
-                        truth_count = truth_diff[key].get(subkey, 0)
-
-                        TP += min(detected_count, truth_count)
-                        FP += max(0, (detected_count - truth_count))
-                        FN += max(0, (truth_count - detected_count))
-
+        aggregated_truth_differences = {}
+        aggregated_detected_differences = {}
                 
+        for timestamp, values in truth_differences.items():
+            # Somma tutti i valori in "incoming" e "outgoing" per il timestamp corrente
+            incoming_sum = sum(values["incoming"].values())
+            outgoing_sum = sum(values["outgoing"].values())
+            
+            # Aggiungi i risultati aggregati al dizionario
+            aggregated_truth_differences[timestamp] = {
+                "incoming": incoming_sum,
+                "outgoing": outgoing_sum
+            }
+        
+        for timestamp, values in detected_differences.items():
+            # Somma tutti i valori in "incoming" e "outgoing" per il timestamp corrente
+            incoming_sum = sum(values["incoming"].values())
+            outgoing_sum = sum(values["outgoing"].values())
+            
+            # Aggiungi i risultati aggregati al dizionario
+            aggregated_detected_differences[timestamp] = {
+                "incoming": incoming_sum,
+                "outgoing": outgoing_sum
+            }
+ 
+        TP, FP, FN = 0, 0, 0
+        # Calcolo delle metriche TP, FP, FN
+        for timestamp in sorted(set(aggregated_detected_differences) | set(aggregated_truth_differences)):
+            detected_diff = aggregated_detected_differences.get(timestamp, {'incoming': 0, 'outgoing': 0})
+            truth_diff = aggregated_truth_differences.get(timestamp, {'incoming': 0, 'outgoing': 0})
+            
+            for key in ['incoming', 'outgoing']:
+                detected_count = detected_diff.get(key, 0)
+                truth_count = truth_diff.get(key, 0)
+
+                # Calculate TP, FP, FN
+                TP += min(detected_count, truth_count)
+                FP += max(0, detected_count - truth_count)
+                FN += max(0, truth_count - detected_count)
+
                 precision = TP / (TP + FP) if (TP + FP) > 0 else 0
                 recall = TP / (TP + FN) if (TP + FN) > 0 else 0
                 f1_score = 2 * (precision * recall) / (precision + recall) if (precision + recall) > 0 else 0
-                
-                try:
-                    total_truth_count = truth[list(truth.keys())[-1]][key][subkey]
-                except KeyError:
-                    total_truth_count = 0
-                try:
-                    total_detected_count = detected[list(detected.keys())[-1]][key][subkey]
-                except KeyError:
-                    total_detected_count = 0
+        
+        # Scrittura dei risultati nel file di valutazione
+        with open(current_path + "/evaluation/" + str(file_path).replace("detected_elements", "evaluation"), 'a') as file:
+            file.write(f"Total True Positives (TP): {TP}\n")
+            file.write(f"Total False Positives (FP): {FP}\n")
+            file.write(f"Total False Negatives (FN): {FN}\n")
+            file.write(f"Aggregated Precision: {precision:.2f}\n")
+            file.write(f"Aggregated Recall: {recall:.2f}\n")
+            file.write(f"Aggregated F1-Score: {f1_score:.2f}\n\n")
 
-                if TP + FN == total_truth_count and TP + FP == total_detected_count:
-                    with open(current_path + "/evaluation/" + str(file_path).replace("detected_elements","evaluation"), 'a') as file:
-
-                        file.write(f"Total truth for {key} {subkey}: {total_truth_count}\n")
-                        file.write(f"Total detected for {key} {subkey}: {total_detected_count}\n")
-                        file.write(f"True Positives (TP) for {key} {subkey}: {TP}\n")
-                        file.write(f"False Positives (FP) for {key} {subkey}: {FP}\n")
-                        file.write(f"False Negatives (FN) for {key} {subkey}: {FN}\n")
-                        file.write(f"Precision for {key} {subkey}: {precision:.2f}\n")
-                        file.write(f"Recall for {key} {subkey}: {recall:.2f}\n")
-                        file.write(f"F1-Score for {key} {subkey}: {f1_score:.2f}\n\n")
-
-                        print("TP, FP OR FN CORRECTLY COMPUTED\n")
-                        print(f"Total truth for {key} {subkey}: {total_truth_count}")
-                        print(f"Total detected for {key} {subkey}: {total_detected_count}")
-                        print(f"True Positives (TP) for {key} {subkey}: {TP}")
-                        print(f"False Positives (FP) for {key} {subkey}: {FP}")
-                        print(f"False Negatives (FN) for {key} {subkey}: {FN}")
-                        print(f"Precision for {key} {subkey}: {precision:.2f}")
-                        print(f"Recall for {key} {subkey}: {recall:.2f}")
-                        print(f"F1-Score for {key} {subkey}: {f1_score:.2f}")
-
-                else:
-                    with open(current_path + "/evaluation/" + str(file_path).replace("detected_elements","evaluation_ERROR"), 'a') as file:
-
-                        file.write(f"Total truth for {key} {subkey}: {total_truth_count}\n")
-                        file.write(f"Total detected for {key} {subkey}: {total_detected_count}\n")
-                        file.write(f"True Positives (TP) for {key} {subkey}: {TP}\n")
-                        file.write(f"False Positives (FP) for {key} {subkey}: {FP}\n")
-                        file.write(f"False Negatives (FN) for {key} {subkey}: {FN}\n\n")
-
-                        print("ERROR WHEN COMPUTING TP, FP OR FN\n")
-                        print(f"Total truth for {key} {subkey}: {total_truth_count}")
-                        print(f"Total detected for {key} {subkey}: {total_detected_count}")
-                        print(f"True Positives (TP) for {key} {subkey}: {TP}")
-                        print(f"False Positives (FP) for {key} {subkey}: {FP}")
-                        print(f"False Negatives (FN) for {key} {subkey}: {FN}")
-
+        # Visualizzazione dei risultati
+        print("Aggregated Evaluation:")
+        print(f"Total True Positives (TP): {TP}")
+        print(f"Total False Positives (FP): {FP}")
+        print(f"Total False Negatives (FN): {FN}")
+        print(f"Aggregated Precision: {precision:.2f}")
+        print(f"Aggregated Recall: {recall:.2f}")
+        print(f"Aggregated F1-Score: {f1_score:.2f}")
 
 def main():
-    """
-    Main method to evaluate model with argument specified via CLI.
-    --video to specify the video file to process.
-    """
-    # Define the CLI arguments
     parser = argparse.ArgumentParser(description="Run YOLO vehicle detection on a video.")
     parser.add_argument(
         "--video", 
@@ -159,15 +108,8 @@ def main():
         choices=['heavy_foggy_road.mp4', 'foggy_road.mp4', 'sunny_road.mp4', 'rainy_road.mp4'],
         help="Video file to process."
     )
-
-
-    # Parse the arguments
     args = parser.parse_args()
-
-    # Call detect_vehicles
     process_files(args.video)
 
 if __name__ == "__main__":
     main()
-                
-       
